7eadfe41a0389087fa2e91ec020d7e073a67541b
Jerry Wan <craftersol728@gmail.com>
2023-05-24 21:03:38 -0700

    added image loop

--- rebecca-dog-grooming/src/App.js
+++ rebecca-dog-grooming/src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import{motion} from 'framer-motion';
 import {useRef, useEffect, useState} from "react";
+import images from './images';
 
 
 function App(){
+   
     return(
         <div className='App'>
-            <motion.h1 animate={{x:50}}>Hello</motion.h1>
+            <motion.div className='carousel'>
+                <motion.div className='inner-carousel'>
+                    {images.map ( image=> {
+                        return(
+                             <motion.div> 
+                                <img src={image} alt='item' />
+                             </motion.div>
+                        );
+                    })}
+                </motion.div>
+            </motion.div>
         </div>
     )
 }
+
+<style>
+    
+</style>
 
 export default App;
