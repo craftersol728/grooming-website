38a692ab68062f5c89ded3e6f234217ca2029d39
Jerry Wan <craftersol728@gmail.com>
2023-05-24 21:38:49 -0700

    finnished image gallary

--- rebecca-dog-grooming/src/App.js
+++ rebecca-dog-grooming/src/App.js
@@ -6,16 +6,27 @@ import images from './images';
 
 
 function App(){
-    
+
     const [width,setWidth] = useState(0);
+    const carousel = useRef();
+
+    useEffect(( )=> {
+        
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    },[])
 
     return(
         <div className='App'>
-            <motion.div className='carousel'>
-                <motion.div drag='x' dragConstraints={{ right:0 }} className='inner-carousel'>
+            <motion.div ref = {carousel} className='carousel'whileTap={{cursor:"grabbing"}}>
+                <motion.div 
+                drag='x' 
+                dragConstraints={{ right:0 , left: -width}} 
+                className='inner-carousel'
+                >
+
                     {images.map ( image=> {
                         return(
-                             <motion.div className='item'> 
+                             <motion.div className='item'key = {image}> 
                                 <img src={image} alt='item' />
                              </motion.div>
                         );
