17c0f6ba662ad0da0de38f8e381431905ab36422
Jerry Wan <craftersol728@gmail.com>
2023-05-25 19:38:10 -0700

    started on navbar and theme toggle button, will change next time

--- rebecca-dog-grooming/src/index.js
+++ rebecca-dog-grooming/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import App from './App';
 import './index.css'
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/navBar';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-      <App />
-        
+      <Navbar />
+      <App /> 
     </>
 );
